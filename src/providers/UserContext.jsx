451674602e9559d9a22ebbf78ff0b5eaeafedb82
ThiagoDuart <td1405150+github@gmail.com>
2023-08-28 15:33:22 -0300

    feat: login request

--- src/providers/UserContext.jsx
+++ src/providers/UserContext.jsx
@@ -1,11 +1,27 @@
 import { createContext } from "react";
+import { apiFeed } from "../services/api";
+import { toast } from "react-toastify";
 
 export const UserContext = createContext({});
 
 export const UserProvider = ({ children }) => {
+  const userLogin = async (dataLogin) => {
+    try {
+      const response = await apiFeed.post("/login", dataLogin);
+      const userData = {
+        userId: response.data.user.id,
+        name: response.data.user.name,
+        token: response.data.accessToken,
+      };
+      localStorage.setItem("@UserData", JSON.stringify(userData));
+    } catch (error) {
+      toast.error("E-mail ou senha incorretos");
+    }
+  };
+
   return (
-    <UserContext.Provider value={{}}>
-    {children}
+    <UserContext.Provider value={{ userLogin }}>
+      {children}
     </UserContext.Provider>
   );
 };
