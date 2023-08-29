eb6bcb4cf05555ae15ffb11abd63eb16fc9007d9
ThiagoDuart <td1405150+github@gmail.com>
2023-08-29 12:43:22 -0300

    Refactor: header footer and dashboardPage

--- src/routes/RoutesMain.jsx
+++ src/routes/RoutesMain.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { RegisterPage } from "../pages/RegisterPage";
 import { LoginPage } from "../pages/LoginPage";
 import { DashboardPage } from "../pages/DashboardPage";
+import { AllPostsPage } from "../pages/AllPostsPage";
 
 export const RoutesMain = () => {
   return (
@@ -9,6 +10,7 @@ export const RoutesMain = () => {
       <Route path="/" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/allPosts" element={<AllPostsPage />} />
       {/* <Route path="*" element={<ErrorPage />} /> */}
     </Routes>
   );
