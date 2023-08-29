85a3ad2a36da0fc5747d97b8170b4e8aaec7ab45
Conrado Augusto <127700916+ConradoAugusto@users.noreply.github.com>
2023-08-29 12:13:02 -0300

    feat: Create ErrorPage

--- src/routes/RoutesMain.jsx
+++ src/routes/RoutesMain.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { RegisterPage } from "../pages/RegisterPage";
 import { LoginPage } from "../pages/LoginPage";
 import { DashboardPage } from "../pages/DashboardPage";
+import { ErrorPage } from "../pages/ErrorPage";
 
 export const RoutesMain = () => {
   return (
@@ -9,7 +10,7 @@ export const RoutesMain = () => {
       <Route path="/" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/dashboard" element={<DashboardPage />} />
-      {/* <Route path="*" element={<ErrorPage />} /> */}
+      <Route path="*" element={<ErrorPage />} />
     </Routes>
   );
 };
