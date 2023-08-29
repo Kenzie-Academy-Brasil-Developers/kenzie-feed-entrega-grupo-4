55f65d456f7d89857f1321b80e1e9287103d0581
Miguel Lucio Silva <miguelluciosilva@gmail.com>
2023-08-29 20:37:29 -0300

    feat: style registerPage

--- src/pages/RegisterPage/index.jsx
+++ src/pages/RegisterPage/index.jsx
@@ -1,19 +1,28 @@
 import { Link } from "react-router-dom";
 import React from "react";
 import { RegisterForm } from "../../components/Forms/RegisterForm";
+import { DefaultTemplate } from "../../components/DefaultTemplate";
+import { MdArrowBack } from "react-icons/md";
+import styles from "./style.module.scss";
 
 export const RegisterPage = () => {
   return (
-    <main>
-      <Link to={"/"}>Voltar</Link>
-      <section>
-        <h1>Cadastre um usuário</h1>
-        <span>Preencha os campos corretamente para fazer login</span>
-
-        <div>
-          <RegisterForm />
+    <DefaultTemplate>
+      <main >
+        <div className={styles.containerBox}>
+          <Link to={"/"} className="btn-outline-register">
+            <MdArrowBack size={24} />
+            <span>Voltar</span>
+          </Link>
+          <section>
+            <h1>Cadastre um usuário</h1>
+            <span>Preencha os campos corretamente para fazer login</span>
+            <div>
+              <RegisterForm />
+            </div>
+          </section>
         </div>
-      </section>
-    </main>
+      </main>
+    </DefaultTemplate>
   );
 };
