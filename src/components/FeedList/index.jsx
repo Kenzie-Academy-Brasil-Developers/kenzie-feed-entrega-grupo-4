d6d778968c685c50bf1fcad2a9b1985425ccd99a
Leandro <leandrolcg18@gmail.com>
2023-08-29 23:18:12 -0300

    DashboardPage atualizada

---
+++ src/components/FeedList/index.jsx
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+import { FeedCard } from "../FeedCard";
+import { PostsContext } from "../../providers/PostsContext";
+
+export const FeedList = () => {
+  const { postList } = useContext(PostsContext);
+
+  return (
+    <ul>
+      {postList.map(post => (
+        <FeedCard key={post.id} post={post}/>
+      ))}
+    </ul>
+  );
+};
--- src/pages/DashboardPage/index.jsx
+++ src/pages/DashboardPage/index.jsx
@@ -1,12 +1,13 @@
 import logo from "../../assets/logo.svg";
 import { MdLogout } from "react-icons/md";
 import { IoMdAddCircleOutline } from "react-icons/io";
-// import { FeedList } from "../../components/FeedList";
+import { FeedList } from "../../components/FeedList";
 import { Footer } from "../../components/Footer";
 import { PostsContext } from "../../providers/PostsContext";
 import { ModalNewPost } from "../../components/Modal/ModalNewPost";
 import { useContext } from "react";
 import { FormNewPost } from "../../components/Forms/NewpostForm";
+import styles from "./style.module.scss";
 
 export const DashboardPage = () => {
   const { isOpenModalNewPost, setIsOpenModalNewPost } =
@@ -14,15 +15,28 @@ export const DashboardPage = () => {
 
   return (
     <>
+      <header className={styles.header__container}>
+        <img src={logo} alt="logo-kz" />
+        <div className={styles.buttons__container}>
+          <img src="#"></img>
+          <button className="btn-outline-mobile">Dashboard</button>
+          <button>
+            <MdLogout title="Logout" aria-label="sair" size={20} />
+          </button>
+        </div>
+      </header>
       {isOpenModalNewPost ? (
         <ModalNewPost>
           <FormNewPost />
         </ModalNewPost>
       ) : null}
       <main>
-        <div>
-          <h1>Suas publicações</h1>
-          <button onClick={() => setIsOpenModalNewPost(true)}>
+        <div className={styles.feedHeader__container}>
+          <h2 className="title-3">Suas publicações</h2>
+          <button
+            onClick={() => setIsOpenModalNewPost(true)}
+            className="btn-solid-s-mobile"
+          >
             <IoMdAddCircleOutline
               title="NewPost"
               aria-label="adicionar post"
@@ -31,7 +45,9 @@ export const DashboardPage = () => {
             Novo post
           </button>
         </div>
-        <div>{/* <FeedList /> */}</div>
+        <div>
+          <FeedList />
+        </div>
       </main>
       <Footer />
     </>
