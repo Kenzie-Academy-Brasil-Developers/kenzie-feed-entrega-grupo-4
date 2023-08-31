f2f7abefc7f1d2c81707ba70e6d58a874b46e7c0
Conrado Augusto <127700916+ConradoAugusto@users.noreply.github.com>
2023-08-31 15:11:29 -0300

    refactor: postCard Styles

--- src/components/FeedList/index.jsx
+++ src/components/FeedList/index.jsx
@@ -1,6 +1,8 @@
 import { useContext } from "react";
 import { FeedCard } from "../FeedCard";
 import { PostsContext } from "../../providers/PostsContext";
+import styles from "./style.module.scss";
+
 
 export const FeedList = () => {
   const { postList } = useContext(PostsContext);
@@ -8,7 +10,7 @@ export const FeedList = () => {
   console.log(postList);
 
   return (
-    <ul>
+    <ul className={styles.container}>
       {postList?.map((post) => (
         <FeedCard key={post.id} post={post} />
       ))}
--- src/components/PostList/PostCard/index.jsx
+++ src/components/PostList/PostCard/index.jsx
@@ -1,21 +1,21 @@
 import styles from "./style.module.scss";
-import { useContext } from "react";
-import { PostsContext } from "../../../providers/PostsContext";
 
 export const PostCard = ({ post }) => {
   return (
     <>
       {post.length == 0 ? (
         <div className={styles.noPost}>
-          <p>Ainda não possui nenhuma publicação</p>
+          <p className="title one">Ainda não possui nenhuma publicação</p>
         </div>
       ) : (
         <li>
           <div className={styles.contentPost}>
-            <img className={styles.img} src={post.image} alt="" />
-            <p className="paragraph-small">Por: {post.owner}</p>
-            <h2 className="title-3">{post.title}</h2>
-            <p className="font-link">Ler mais</p>
+            <div className={styles.containerBanner}>
+              <img className={styles.banner} src={post.image} alt="" />
+            </div>
+            <p className="paragraph small">Por: {post.owner}</p>
+            <h2 className="title three">{post.title}</h2>
+            <p className="paragraph link blue">Ler mais</p>
           </div>
         </li>
       )}
