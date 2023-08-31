04aca70bb24e1b699a1768c31dbc5177374ba406
DanielVrs <danielvrsdev@gmail.com>
2023-08-31 10:43:06 -0300

    refac: cardList and card

--- src/components/PostList/PostCard/index.jsx
+++ src/components/PostList/PostCard/index.jsx
@@ -2,29 +2,23 @@ import styles from "./style.module.scss";
 import { useContext } from "react";
 import { PostsContext } from "../../../providers/PostsContext";
 
-export const PostCard = () => {
-  const { postList } = useContext(PostsContext);
-  const posts = postList || [];
-
+export const PostCard = ({ post }) => {
   return (
     <>
-      {!posts.length && (
+      {post.length == 0 ? (
         <div className={styles.noPost}>
           <p>Ainda não possui nenhuma publicação</p>
         </div>
+      ) : (
+        <li>
+          <div className={styles.contentPost}>
+            <img className={styles.img} src={post.image} alt="" />
+            <p className="paragraph-small">Por: {post.owner}</p>
+            <h2 className="title-3">{post.title}</h2>
+            <p className="font-link">Ler mais</p>
+          </div>
+        </li>
       )}
-      {posts?.map((post) => {
-        return (
-          <li key={post.id}>
-            <div className={styles.contentPost}>
-              <img className={styles.img} src={post.image} alt="" />
-              <p className="paragraph-small">Por: {post.owner}</p>
-              <h2 className="title-3">{post.title}</h2>
-              <p className="font-link">Ler mais</p>
-            </div>
-          </li>
-        );
-      })}
     </>
   );
 };
--- src/components/PostList/index.jsx
+++ src/components/PostList/index.jsx
@@ -1,10 +1,16 @@
+import { useContext } from "react";
 import { PostCard } from "./PostCard";
 import style from "./style.module.scss";
+import { PostsContext } from "../../providers/PostsContext";
 
 export const PostList = () => {
+  const { postList } = useContext(PostsContext);
+
   return (
     <ul className={style.postList}>
-      <PostCard />
+      {postList?.map((post) => (
+        <PostCard key={post.id} post={post} />
+      ))}
     </ul>
   );
 };
