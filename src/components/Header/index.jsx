a565d8c93e59b77b292c7cca6539f898a5cbfd48
Miguel Lucio Silva <miguelluciosilva@gmail.com>
2023-09-01 16:26:25 -0300

    refactor: header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,14 +3,26 @@ import Logo from "../../assets/logo.svg";
 import styles from "./style.module.scss";
 
 export const Header = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  console.log(window.location.pathname);
+  const location = window.location.pathname;
+
   return (
     <div className="container">
       <header>
         <div className={styles.headerBox}>
           <img src={Logo} alt="LogoKenzieFeed" onClick={() => navigate("/")} />
-          <button className="btn-small" onClick={() => navigate("/loginPage")} >Acessar</button>
-        </div> 
+          <button
+            className="btn-small"
+            onClick={() =>
+              location === "/loginPage"
+                ? navigate("/register")
+                : navigate("/loginPage")
+            }
+          >
+            {location === "/loginPage" ? "Cadastre-se" : "Acessar"}
+          </button>
+        </div>
       </header>
     </div>
   );
