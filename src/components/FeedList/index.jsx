fdd10017d9c47cd31a15ed08a25709187b1b5377
Conrado Augusto <127700916+ConradoAugusto@users.noreply.github.com>
2023-09-02 11:06:20 -0300

    refactor: ternary dashbordPost

--- src/components/FeedList/index.jsx
+++ src/components/FeedList/index.jsx
@@ -6,10 +6,9 @@ export const FeedList = () => {
   const { postList, setIsOpenModalNewPost } = useContext(PostsContext);
   const { userId } = JSON.parse(localStorage.getItem("@UserData"));
   const newPostList = postList?.filter((post) => post.userId === userId);
-  console.log(newPostList);
   return (
     <ul>
-      {newPostList.lenght === 0 ? ( newPostList?.map((post) => (
+      {newPostList.length !== 0 ? ( newPostList?.map((post) => (
         <FeedCard key={post.id} post={post} />
       ))) : (<li onClick={() => setIsOpenModalNewPost(true)} className="paragraph line"><p className="paragraph bold blue textCenter">Você ainda não possui publicações</p></li>)}
      
